357a57f9e79a03a9cba7f7445985022a15b0625c
ishai2s4 <ishai2s4@gmail.com>
2020-10-12 16:44:17 +0530

    Updates

---
+++ precache-manifest.618a6f681c22f77b24dafdfd3014211b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d80eb57444d647ac2153662927e99b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "775e7023345fc8f6e814",
+    "url": "/portfolio/static/css/main.3be05411.chunk.css"
+  },
+  {
+    "revision": "905c0a06cb8a16f5a30d",
+    "url": "/portfolio/static/js/2.ea02c3c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.ea02c3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775e7023345fc8f6e814",
+    "url": "/portfolio/static/js/main.03a0b3fb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
